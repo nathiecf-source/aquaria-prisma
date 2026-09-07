0dd368368dfc565fb3350aa46cf4c95980515c20
nathiecf <nathiecf@gmail.com>
2026-09-06 21:57:13 -0300

    chore: remove scripts de teste antigos da ProKerala

--- test-prokerala.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-import('./dist/server.mjs').then((mod) => {
-  // Adaptar se callProKeralaAPI for exposto pelo servidor.
-  console.log(mod);
-});
